29736b5eabb246f7324c5ef5293f1463ceee6b36
Shubh Sheth <shubhsheth8@gmail.com>
2021-05-27 00:33:12 -0500

    Device Changes + Refactoring

---
+++ functions/utils/utils.js
@@ -0,0 +1,140 @@
+const functions = require('firebase-functions');
+
+
+// Initialize Firebase
+const admin = require('firebase-admin');
+admin.initializeApp();
+const firebaseRef = admin.database().ref('/');
+
+
+// App Data
+const USER_ID = '123';
+const devices = JSON.parse(JSON.stringify(require('../devices.json')));
+
+
+// Functions
+const onSync = (body) => {
+	return {
+		requestId: body.requestId,
+		payload: {
+			agentUserId: USER_ID,
+			devices: devices,
+		},
+	};
+}
+
+const onDisconnect = (body, headers) => {
+	functions.logger.log('User account unlinked from Google Assistant');
+	// Return empty response
+	return {};
+}
+
+const onQuery = async (body) => {
+	const { requestId } = body;
+	const payload = {
+		devices: {},
+	};
+	const queryPromises = [];
+	const intent = body.inputs[0];
+	for (const device of intent.payload.devices) {
+		const deviceId = device.id;
+		queryPromises.push(queryDevice(deviceId)
+			.then((data) => {
+				// Add response to device payload
+				payload.devices[deviceId] = data;
+			}
+			));
+	}
+	// Wait for all promises to resolve
+	await Promise.all(queryPromises);
+	return {
+		requestId: requestId,
+		payload: payload,
+	};
+}
+
+const onExecute = async (body) => {
+	const { requestId } = body;
+	// Execution results are grouped by status
+	const result = {
+		ids: [],
+		status: 'SUCCESS',
+		states: {
+			online: true,
+		},
+	};
+
+	const executePromises = [];
+	const intent = body.inputs[0];
+	for (const command of intent.payload.commands) {
+		for (const device of command.devices) {
+			for (const execution of command.execution) {
+				executePromises.push(
+					updateDevice(execution, device.id)
+						.then((data) => {
+							result.ids.push(device.id);
+							Object.assign(result.states, data);
+						})
+						.catch(() => functions.logger.error('EXECUTE', device.id)));
+			}
+		}
+	}
+
+	await Promise.all(executePromises);
+	return {
+		requestId: requestId,
+		payload: {
+			commands: [result],
+		},
+	};
+}
+
+const queryFirebase = async (deviceId) => {
+	const snapshot = await firebaseRef.child(deviceId).once('value');
+	const snapshotVal = snapshot.val();
+	return {
+		on: snapshotVal.OnOff.on,
+		isPaused: snapshotVal.StartStop.isPaused,
+		isRunning: snapshotVal.StartStop.isRunning,
+	};
+};
+
+const queryDevice = async (deviceId) => {
+	const data = await queryFirebase(deviceId);
+	return {
+		on: data.on,
+		isPaused: data.isPaused,
+		isRunning: data.isRunning,
+		currentRunCycle: [{
+			currentCycle: 'rinse',
+			nextCycle: 'spin',
+			lang: 'en',
+		}],
+		currentTotalRemainingTime: 1212,
+		currentCycleRemainingTime: 301,
+	};
+};
+
+const updateDevice = async (execution, deviceId) => {
+	const { params, command } = execution;
+	let state; let ref;
+	switch (command) {
+		case 'action.devices.commands.OnOff':
+			state = { on: params.on };
+			ref = firebaseRef.child(deviceId).child('OnOff');
+			break;
+		case 'action.devices.commands.StartStop':
+			state = { isRunning: params.start };
+			ref = firebaseRef.child(deviceId).child('StartStop');
+			break;
+		case 'action.devices.commands.PauseUnpause':
+			state = { isPaused: params.pause };
+			ref = firebaseRef.child(deviceId).child('StartStop');
+			break;
+	}
+
+	return ref.update(state)
+		.then(() => state);
+};
+
+module.exports = { onSync, onDisconnect, onQuery, onExecute };
